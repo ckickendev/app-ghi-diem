02358238bacb9a52a1fda86ab0e8f166b4db7a37
gintaka <thesoonafu@gmail.com>
2025-02-12 00:38:19 +0700

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/(tabs)/_layout.tsx",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
